3ef9a232175562699cb4e77a39ca4abc1c5f262c
James McCann <jamesredux@gmail.com>
2017-04-07 22:10:51 +0700

    pig latin completed

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,45 @@
-#write your code here
+
+def translate(str)
+
+  consonants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+
+  str_arr =  str.split
+
+  output = ""
+  str2 = "" 
+
+  str_arr.each do |word|
+
+ 
+  if word[0]=="q" && word[1]=="u"
+    
+    word <<word.slice!(0..1)
+    output += word + "ay "
+
+  elsif word[0]=="s" && word[1]=="q" && word[2]=="u"
+    
+    word <<word.slice!(0..2)
+    output += word + "ay "
+      
+
+    else
+    while consonants.include? word[0]
+     
+    str2 += word.slice!(0) 
+    end
+    output += word + str2 + "ay "
+    
+    str2 = ""
+    
+
+end
+
+    
+    
+  end
+
+output.strip
+
+end 
+
+puts translate('aquarium square this is a test quiet')
