0fd52261ee2e2cfc6b45f6e1883b2bd628f6d877
James McCann <jamesredux@gmail.com>
2017-04-09 11:44:09 +0700

    completed problems 5 and 6

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,32 @@
 class Book
-# write your code here
+
+attr_accessor :title
+
+def title=(book_title)
+
+		words = book_title.split
+		littleuns = ['if','and','over','it','the','a','an','or','but','for','on','is','of','in'i]
+
+		words.each do |x|
+			if littleuns.include?(x)
+				x
+					
+		else
+			x.capitalize!	
+		end	
+	
 end
+		words[0].capitalize!
+		words = words.join(" ")
+		puts words
+		@title=words
+	end
+	
+end
+
+
+inferno = Book.new
+inferno.title = 'i think i should be in the best of the best which'
+
+
+
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,45 @@
 class Timer
-  #write your code here
+
+attr_accessor :seconds	
+
+	
+	def initialize
+
+		@seconds = 0
+
+	end	
+
+	def padded(n)
+		if n < 10  
+		n =  "0" + n.to_s
+
+		else 
+			n = n.to_s  
+		end	
+
+	end		
+
+
+	def time_string
+		sec = @seconds
+		hrs = sec/3600
+		sec = sec-(3600*hrs)
+
+		mins = sec/60
+		sec = sec-(60*mins)
+		
+	
+	return	"#{padded(hrs)}:#{padded(mins)}:#{padded(sec)}"
+		
+
+
+		
+	end
+
 end
+
+
+clock = Timer.new
+clock.seconds = 90
+puts clock.time_string
+
